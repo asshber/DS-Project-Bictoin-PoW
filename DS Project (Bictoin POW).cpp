082390feccb37fbba0fc816da5f7964432b6be7c
Ali Ashber <k201051@nu.edu.pk>
2021-11-28 21:31:10 +0500

    Hashing done

--- DS Project (Bictoin POW).cpp
+++ DS Project (Bictoin POW).cpp
@@ -1,7 +1,33 @@
 #include<iostream>
 #include<vector>
+#include<string>
+#include<sstream>
+#include"SHA256.h"
 using namespace std;
-class B_Node
+class Blockchain_Node;
+vector<Blockchain_Node> Blockchain;
+class Blockchain_Node
 {
-
+	string p_hash;
+	string data;
+	string hash;
+public:
+	Blockchain_Node(string x):data(x)
+	{
+		stringstream s;
+		p_hash = getlasthash();
+		s << data << p_hash;
+		hash = sha256(s.str());
+		p_hash = getlasthash();
+	}
+	string getlasthash()
+	{
+		Blockchain_Node p = Blockchain.back();
+		return (p.hash);
+	}
 };
+
+int main()
+{
+	
+}
