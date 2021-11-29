02fe416d8c25af3ca2bd3ace1420d1cf101b4655
Ali Ashber <k201051@nu.edu.pk>
2021-11-29 19:40:22 +0500

    Puzzle Solved

--- DS Project (Bictoin POW).cpp
+++ DS Project (Bictoin POW).cpp
@@ -2,32 +2,75 @@
 #include<vector>
 #include<string>
 #include<sstream>
+#include <chrono>
 #include"SHA256.h"
 using namespace std;
-class Blockchain_Node;
-vector<Blockchain_Node> Blockchain;
-class Blockchain_Node
+//class Blockchain_Node;
+//vector<Blockchain_Node> Blockchain;
+struct Details
 {
-	string p_hash;
-	string data;
-	string hash;
+	//string name;
+	//string time;
+	int nonce;
+	Details(int x):nonce(x){}
+};
+stringstream& operator<<(stringstream& dout,Details& obj)
+{
+	//dout << obj.name << obj.time;
+	dout<< obj.nonce;
+	return dout;
+}
+class Miner
+{
+	string name;
+	int count;
 public:
-	Blockchain_Node(string x):data(x)
+	void solve(string puzzle,string data)
 	{
 		stringstream s;
-		p_hash = getlasthash();
-		s << data << p_hash;
-		hash = sha256(s.str());
-		p_hash = getlasthash();
-	}
-	string getlasthash()
-	{
-		Blockchain_Node p = Blockchain.back();
-		return (p.hash);
+		string hash;
+		for (int i = 0; i < 10000; i++)
+		{
+			s << i;
+			s << data;
+			hash = sha256(s.str());
+			if (hash == puzzle)
+			{
+				cout << "pp";
+				break;
+			}
+			s.str("");
+		}
 	}
 };
+//class Blockchain_Node
+//{
+//	string p_hash;
+//	Details data;
+//	string hash;
+//public:
+//	Blockchain_Node()
+//	{
+//		stringstream s;
+//		p_hash = getlasthash();
+//		s << data<<p_hash;
+//		hash = sha256(s.str());
+//		p_hash = getlasthash();
+//		Blockchain.push_back(*this);
+//	}
+//	string getlasthash()
+//	{
+//		Blockchain_Node p = Blockchain.back();
+//		return (p.hash);
+//	}
+//};
 
 int main()
 {
-	
+	stringstream s;
+	s << 2;
+	s << "hello";
+	string hash = sha256(s.str());
+	Miner obj;
+	obj.solve(hash, "hello");
 }
